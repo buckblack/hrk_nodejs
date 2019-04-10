89afc5465267afc8386f72d5c3705cf003027ef2
buckblack <42344854+buckblack@users.noreply.github.com>
2019-04-10 09:06:18 +0700

    .

--- routes/users.js
+++ routes/users.js
@@ -104,8 +104,58 @@ router.post('/don-hang/chi-tiet',async function(req, res, next) {
     res.json(sanpham[0].chi_tiet)
   });
 });
-router.get('/don-hang/:id', function(req, res, next) {
-  res.render('users/chitietdonhang',{tieude:'Gear Srore | Chi tiết đơn hàng',trangthai:'Chi tiết đơn hàng',madonhang:req.params.id});
+
+router.get('/in-hoa-don/:id',async function(req, res, next) {
+  let db = await xl_mongo.Get();
+  db.collection(cl_hoa_don).aggregate([
+    {
+      $match: { "_id": ObjectId(req.params.id) }
+    },
+    {
+      $lookup: {
+        from: 'nguoi_dung',
+        localField: 'khach_hang',
+        foreignField: '_id',
+        as: 'khach_hang'
+      }
+    },
+    {
+      $unwind: {
+        path: "$chi_tiet",
+        preserveNullAndEmptyArrays: true
+      }
+    },
+    {
+      $lookup: {
+        from: "san_pham",
+        localField: "chi_tiet.san_pham",
+        foreignField: "_id",
+        as: "chi_tiet.san_pham"
+      }
+    },
+    {
+      $unwind: {
+        path: "$chi_tiet.san_pham",
+        preserveNullAndEmptyArrays: true
+      }
+    },
+    {
+      $group: {
+        _id: "$_id",
+        khach_hang: {
+          "$first": "$khach_hang"
+        },
+        ma_hd: {
+          "$first": "$ma_hd"
+        },
+        chi_tiet: {
+          "$push": "$chi_tiet"
+        }
+      }
+    }
+  ]).toArray((error,result)=>{
+    res.render('users/in_hoa_don',{hoadon: result});
+  });
 });
 
 module.exports = router;
